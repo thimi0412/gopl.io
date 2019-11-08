0a74a6c1474ad931119fe43ce9af511e8b629e42
thimi0412 <thimi0412@icloud.com>
2019-11-08 17:23:20 +0900

    update

---
+++ ch2/q2/conv/temp.go
@@ -0,0 +1,25 @@
+package tempconv
+
+import "fmt"
+
+// Celsius .
+type Celsius float64
+
+// Fahrenheit .
+type Fahrenheit float64
+
+//  C
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g˚C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g˚F", f) }
+
+// CToF convert celsius to fahrenheit .
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC convert fahrenheit to celsius .
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
